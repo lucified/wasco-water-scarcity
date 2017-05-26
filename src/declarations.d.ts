18da545dd5f8a3ec35a6b34eeb9a16deffe0d41c
Ville Saarinen <ville.saarinen@gmail.com>
2017-05-26 12:52:51 +0300

    Initial commit

---
+++ src/declarations.d.ts
@@ -0,0 +1,22 @@
+// These are needed to tell TypeScript what the imports for
+// these file types return.
+
+declare module '*.scss' {
+  const value: any;
+  export = value;
+}
+
+declare module '*.svg' {
+  const content: string;
+  export = content;
+}
+
+declare module '*.png' {
+  const content: string;
+  export = content;
+}
+
+declare module '*.hbs' {
+  const content: (context: any) => string;
+  export = content;
+}
