32092a885b8a24a326b13a759aac0295769463ea
Ville Saarinen <ville.saarinen@gmail.com>
2017-05-30 15:00:39 +0300

    Switch to UglifyJS from Babili

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const autoprefixer = require('autoprefixer');
-const BabiliPlugin = require('babili-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const postCssFlexbugsFixer = require('postcss-flexbugs-fixes');
@@ -184,7 +183,22 @@ if (['production', 'staging'].indexOf(deployConfig.env) > -1) {
       minimize: true,
       debug: false,
     }),
-    new BabiliPlugin(),
+    // Minify the code.
+    // TODO: Switch to BabiliPlugin in order to get tree-shaking at some point.
+    // It understands ES6 imports while (as of now) UglifyJs doesn't.
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        // This feature has been reported as buggy a few times, such as:
+        // https://github.com/mishoo/UglifyJS2/issues/1964
+        // We'll wait with enabling it by default until it is more solid.
+        reduce_vars: false,
+      },
+      output: {
+        comments: false,
+      },
+      sourceMap: true,
+    }),
   ]);
 }
 
