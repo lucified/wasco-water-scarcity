aad954f4faa18366e714473919f4c6dfe83ed3ee
Ville Saarinen <ville.saarinen@gmail.com>
2017-07-24 12:10:01 +0300

    Add a disclaimer

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,9 @@
+import * as classNames from 'classnames';
 import * as React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const aaltoLogo = require('../../images/aalto_logo.svg');
-const menuIcon = require('../../images/hamburger.svg');
+// const menuIcon = require('../../images/hamburger.svg');
 const styles = require('./index.scss');
 
 export default class Header extends React.Component {
@@ -11,14 +12,29 @@ export default class Header extends React.Component {
       <div className={styles.root}>
         <div className="container">
           <div className="row between-xs">
-            <div className={styles['left-content']}>
+            <div
+              className={classNames(
+                'col-xs-12',
+                'col-md-8',
+                styles['left-content'],
+              )}
+            >
               <img src={aaltoLogo} className={styles.logo} />
               <div className={styles.heading}>Water Scarcity Atlas</div>
             </div>
-            <div className={styles.menu}>
+            <div
+              className={classNames('col-xs-12', 'col-md-4', styles.disclaimer)}
+            >
+              <div className={styles.label}>Prototype</div>
+              <div>
+                The visualizations on this page are experimental and unpolished.
+                Mobile devices are not yet supported.
+              </div>
+            </div>
+            {/*<div className={styles.menu}>
               <img src={menuIcon} />
               <div className={styles['menu-text']}>All themes</div>
-            </div>
+            </div>*/}
           </div>
         </div>
         <div className={styles.navigation}>
