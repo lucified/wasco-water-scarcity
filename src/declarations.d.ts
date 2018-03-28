1b0bd3be07e31e8ce42134af203b4833ad9f6d20
Ville Saarinen <ville.saarinen@gmail.com>
2018-03-28 17:36:25 +0300

    Manual declarations no longer needed

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -20,11 +20,3 @@ declare module '*.hbs' {
   const content: (context: any) => string;
   export = content;
 }
-
-// These are missing in official typings
-declare module 'd3-scale-chromatic' {
-  const schemeReds: string[][];
-  const schemeBlues: string[][];
-  const schemePurples: string[][];
-  const schemeOranges: string[][];
-}
