2ab076c09640d00ab021fd931feeadafdc848b1e
Ville Saarinen <ville.saarinen@gmail.com>
2018-04-28 10:07:11 +0100

    Revert to BrowserRouter

--- src/embed-entry.tsx
+++ src/embed-entry.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { AppEmbed } from './components/app-embed';
 import configureStore from './configure-store';
 import { initialState } from './reducers';
@@ -10,9 +10,9 @@ const store = configureStore(initialState);
 
 ReactDOM.render(
   <Provider store={store}>
-    <HashRouter>
+    <BrowserRouter>
       <AppEmbed />
-    </HashRouter>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('content'),
 );
